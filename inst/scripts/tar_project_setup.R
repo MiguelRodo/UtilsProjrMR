3751d86cd6a1ee4653786bdaac7481c16ef72e84
Miguel Julio Rodo <rdxmig002@myuct.ac.za>
2023-01-19 09:42:39 +0200

    Refine tar_project_setup.R

--- inst/scripts/tar_project_setup.R
+++ inst/scripts/tar_project_setup.R
@@ -1,9 +1,7 @@
-tar_project_set_up <- function(proj_nm) {
+projr_tar_project_set_up <- function(proj_nm) {
   library(targets)
-  project_yml <- yaml::read_yaml(here::here("_projr.yml"))
   # set project store and script
-  dir_proj <- file.path(projr_dir_get("cache"), "targets", proj_nm)
-  if (!dir.exists(dir_proj)) dir.create(dir_proj, recursive = TRUE)
+  dir_proj <- projr_dir_get("cache", "targets", proj_nm)
   targets::tar_config_set(
     script = file.path(dir_proj, "_targets.R"),
     store = file.path(dir_proj, "_targets"),
@@ -12,3 +10,4 @@ tar_project_set_up <- function(proj_nm) {
   Sys.setenv("TAR_PROJECT" = proj_nm)
   invisible(TRUE)
 }
+projr_tar_project_setup <- projr_tar_project_set_up
