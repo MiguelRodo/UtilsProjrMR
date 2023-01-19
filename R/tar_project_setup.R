a1657b0c43af903ab2ca69d90676d4bdcb33cdff
Miguel Julio Rodo <rdxmig002@myuct.ac.za>
2023-01-19 11:01:25 +0200

    Depend on projr properly

--- R/tar_project_setup.R
+++ R/tar_project_setup.R
@@ -17,7 +17,7 @@ projr_tar_project_set_up <- function(proj_nm) {
   }
   library(targets)
   # set project store and script
-  dir_proj <- projr_dir_get("cache", "targets", proj_nm)
+  dir_proj <- projr::projr_dir_get("cache", "targets", proj_nm)
   targets::tar_config_set(
     script = file.path(dir_proj, "_targets.R"),
     store = file.path(dir_proj, "_targets"),
