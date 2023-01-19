5c54122a9bfbe6526b9481cf7a26822081b47cf1
Miguel Julio Rodo <rdxmig002@myuct.ac.za>
2023-01-19 11:03:49 +0200

    Improve and export projr_init_extra

--- R/init_extra.R
+++ R/init_extra.R
@@ -1,5 +1,14 @@
+#' @title Add extra bits to project set-up
+#'
+#' @description
+#' Makes .Rprofile set up renv for HPC and to use binaries,
+#' attaches useful functions and packages and installs
+#' packages useful for VS Code.
+#'
+#' @export
 projr_init_extra <- function() {
   projr_rprofile_hpc_renv_setup()
+  projr_rprofile_renv_repos()
   projr_rprofile_dev()
   projr_renv_dep_add(c("languageserver", "httpgd", "devtools", "jsonlite"))
   invisible(TRUE)
