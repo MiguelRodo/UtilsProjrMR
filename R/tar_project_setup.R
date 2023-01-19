015486e2cef8ab9c04baf6979d509e775f8fe862
Miguel Julio Rodo <rdxmig002@myuct.ac.za>
2023-01-19 11:23:44 +0200

    Add _targets.R file

--- R/tar_project_setup.R
+++ R/tar_project_setup.R
@@ -15,6 +15,12 @@ projr_tar_project_set_up <- function(proj_nm) {
   if (!requireNamespace("projr", quietly = TRUE)) {
     renv::install("SATVILab/projr")
   }
+  if (!requireNamespace("ggplot2", quietly = TRUE)) {
+    renv::install("ggplot2")
+  }
+  if (!requireNamespace("tibble", quietly = TRUE)) {
+    renv::install("tibble")
+  }
   library(targets)
   # set project store and script
   dir_proj <- projr::projr_dir_get("cache", "targets", proj_nm)
@@ -23,7 +29,16 @@ projr_tar_project_set_up <- function(proj_nm) {
     store = file.path(dir_proj, "_targets"),
     project = proj_nm
   )
+  if (!file.exists(file.path(dir_proj, "_targets.R"))) {
+    file.copy(
+      system.file("scripts", "_targets.R", package = "UtilsProjrMR"),
+      file.path(dir_proj, "_targets.R")
+    )
+  }
   Sys.setenv("TAR_PROJECT" = proj_nm)
+  for (x in list.files(here::here("R"), pattern = "R$|r$", full.names = TRUE)) {
+    source(x)
+  }
   invisible(TRUE)
 }
 #' @export
---
+++ inst/scripts/_targets.R
@@ -0,0 +1,31 @@
+# _targets.R file
+library(targets)
+for (x in list.files(here::here("R"), pattern = "R$|r$", full.names = TRUE)) {
+  source(x)
+}
+targets::tar_option_set(
+  # attach packages in targets
+  packages = c("ggplot2", "tibble")
+)
+for (x in list.files("R", full.names = TRUE)) source(x)
+list(
+  # specify _projr directories
+  # ------------------
+  tar_target(
+    dir_data_raw, projr_dir_get("data-raw"),
+    cue = tar_cue(mode = "always")
+  ),
+  tar_target(
+    dir_cache, projr_dir_get("cache"),
+    cue = tar_cue(mode = "always")
+  ),
+  tar_target(
+    dir_output, projr_dir_get("output"),
+    cue = tar_cue(mode = "always")
+  ),
+  # do something
+  # ------------------
+  tar_target(x, {
+    1
+  })
+)
