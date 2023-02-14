3e058e46e13de2a8fc7e4578aadfd7941539d61a
Miguel Julio Rodo <rdxmig002@myuct.ac.za>
2023-02-14 07:30:39 +0200

    Install httpgd off CRAN

--- R/init_extra.R
+++ R/init_extra.R
@@ -10,6 +10,6 @@ projr_init_extra <- function() {
   projr_rprofile_hpc_renv_setup()
   projr_rprofile_renv_repos()
   projr_rprofile_dev()
-  projr_renv_dep_add(c("languageserver", "nx10/httpgd", "devtools", "jsonlite"))
+  projr_renv_dep_add(c("languageserver", "httpgd", "devtools", "jsonlite"))
   invisible(TRUE)
 }
