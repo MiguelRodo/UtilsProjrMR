8cee304e8cae14ae69c1fba3dba9c2786265f18a
Miguel Julio Rodo <rdxmig002@myuct.ac.za>
2023-03-03 16:07:18 +0200

    Don't ensure renv repos have binary as projr does that

--- R/init_extra.R
+++ R/init_extra.R
@@ -8,7 +8,6 @@
 #' @export
 projr_init_extra <- function() {
   projr_rprofile_hpc_renv_setup()
-  projr_rprofile_renv_repos()
   projr_rprofile_dev()
   projr_renv_dep_add(c("languageserver", "httpgd", "devtools", "jsonlite"))
   invisible(TRUE)
