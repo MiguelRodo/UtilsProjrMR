d05004894b2815cfafd130521989bbd62b143a80
Miguel Rodo <rdxmig002@myuct.ac.za>
2023-07-08 12:26:03 +0200

    Don't add hpc set up file

--- R/init_extra.R
+++ R/init_extra.R
@@ -7,7 +7,6 @@
 #'
 #' @export
 projr_init_extra <- function() {
-  projr_rprofile_hpc_renv_setup()
   projr_rprofile_dev()
   projr_renv_dep_add(c("languageserver", "httpgd", "devtools", "jsonlite"))
   invisible(TRUE)
