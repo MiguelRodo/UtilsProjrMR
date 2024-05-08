b29775241415a20641c893f7bb1d15042c3fd83b
Miguel Rodo <rdxmig002@myuct.ac.za>
2024-05-08 17:28:58 +0200

    Add renv_restore_update

--- R/projr_renv_dep_add.R
+++ R/projr_renv_dep_add.R
@@ -33,3 +33,28 @@ projr_renv_dep_add <- function(pkg) {
   writeLines(txt_dep, "_dependencies.R")
   invisible(TRUE)
 }
+
+#' @title Restore
+projr_renv_restore_update <- function(restore_gh = TRUE) {
+  lockfile_list_pkg <- renv::lockfile_read()$Package
+  pkg_vec <- NULL
+  for (i in seq_along(lockfile_list_pkg)) {
+    pkg_list <- lockfile_list_pkg[[i]]
+    pkg <- pkg_list$Package
+    remote_username <- pkg_list$RemoteUsername
+    if (is.null(remote_username)) {
+      pkg_vec <- c(pkg, pkg_vec)
+    } else {
+      pkg_vec <- c(pkg_vec, paste0(remote_username, "/", pkg))
+    }
+  }
+  if (restore_gh) {
+    pkg_vec_non_gh <- pkg_vec[!grepl("/", pkg_vec)]
+    pkg_vec_gh <- pkg_vec[grepl("/", pkg_vec)]
+    renv::install(pkg_vec_non_gh)
+    renv::restore(pkg_vec_gh)
+  } else {
+    renv::install(pkg_vec)
+  }
+  invisible(TRUE)
+} 
